74d80f1feb7a9ffb7a65c3f94238f353648f6fe1
otavio-uni <2222760@aluno.univesp.br>
2024-04-09 02:15:05 -0300

    Responsividade: início de escrita.
    Iniciando esboços de código com responsividade css.

--- docs/javaScript.js
+++ docs/javaScript.js
@@ -1,22 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const tabs = document.querySelectorAll('.tabs .tab');
-  
-    tabs.forEach(tab => {
-      tab.addEventListener('click', function(event) {
-        event.preventDefault(); // Impede a ação padrão do link
-  
-        // Oculta todos os conteúdos das abas e remove a classe 'active' das abas
-        document.querySelectorAll('.tab-content').forEach(tabContent => {
-          tabContent.classList.remove('active');
-        });
-        document.querySelectorAll('.tab').forEach(tab => {
-          tab.classList.remove('active');
-        });
-  
-        // Exibe o conteúdo da aba selecionada e adiciona a classe 'active' à aba
-        const tabName = this.getAttribute('data-tab');
-        document.getElementById(tabName).classList.add('active');
-        this.classList.add('active');
+  const tabs = document.querySelectorAll('.tabsMain .tab'); // Corrigindo a seleção dos elementos .tab
+
+  tabs.forEach(tab => { // Corrigindo para 'tabs.forEach'
+    tab.addEventListener('click', function(event) {
+      event.preventDefault(); // Impede a ação padrão
+
+      // Oculta todos os conteúdos das abas
+      document.querySelectorAll('.tab-content').forEach(tabContent => {
+        tabContent.classList.remove('active');
       });
+
+      // Exibe o conteúdo da aba selecionada
+      const tabName = this.getAttribute('data-tab');
+      document.getElementById(tabName).classList.add('active');
     });
   });
+});
