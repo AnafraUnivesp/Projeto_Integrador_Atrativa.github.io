064368807175a645cdf870fc6b5661a35716737b
unknown <2202961@aluno.univesp.br>
2024-05-05 16:01:05 -0300

    evoluindo site

--- webserver/static/js/script.js
+++ webserver/static/js/script.js
@@ -5,7 +5,7 @@ const body = document.querySelector('body');
 
 menuMobile.addEventListener("click", () => {
     menuMobile.classList.contains("bi-list")
-    ? menuMobile.classList.contains("bi-list", "bi-x") /* i e else ternario*/ 
-    : menuMobile.classList.replace("bi-x", "bi-list");
+    ? menuMobile.classList.contains("bi-list", "bi-close") /* i e else ternario*/ 
+    : menuMobile.classList.replace("bi-close", "bi-list");
 body.classList.toggle("menu-nav-active")
 });
