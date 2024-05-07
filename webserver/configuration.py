dd3f37cf5e0cac73cd6509baf85b288d5da2ac8f
Thais Faustino <thaisfsouza@outlook.com>
2024-05-07 18:03:56 -0300

    Apaga arquivos nao utilizados

--- webserver/configuration.py
+++ webserver/configuration.py
@@ -1,6 +1,5 @@
 from routes.home import home_route
 from routes.camiseta import camiseta_route
-from routes.camisa import camisa_route
 from database.database import db
 from database.models.camiseta import Camiseta
 
@@ -11,7 +10,6 @@ def configure_all(app):
 def configure_routes(app):
     app.register_blueprint(home_route) 
     app.register_blueprint(camiseta_route, url_prefix='/camiseta') 
-    app.register_blueprint(camisa_route, url_prefix='/camisa') 
 
 def configure_db():
     db.connect()
--- webserver/database/camisa.py
+++
@@ -1,7 +0,0 @@
-
-CAMISA = [
-    {"id":1,"item": "camisa","tipo":"masculina","modelo": "regata", "marca": "prada","cor": "azul", "tamanho": "pp","detalhe": "outono_inverno"},
-    
-
-]
-                                                    
